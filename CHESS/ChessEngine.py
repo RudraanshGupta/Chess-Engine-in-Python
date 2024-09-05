d23195416cb6c065fd4351820156b41800df6768
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-09-05 17:30:25 +0530

    making general moves

--- CHESS/ChessEngine.py
+++ CHESS/ChessEngine.py
@@ -10,3 +10,16 @@ class gamestate():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+
+    def makeMoves(self, move):
+        if len(self.movelog)!=0:
+            self.board[move.endRow][move.endCol] = move.pieceMoved
+            self.board[move.startRow][move.startCol] = "--"
+            self.movelog.append(move)
+            self.whitetomove = not self.whitetomove  # Toggle turn
+            print("Move made:", move.getChessnotation())
+            self.printBoard()
+            if move.pieceMoved == 'wK':
+                self.whiteKingLocation = (move.endRow, move.endCol)
+            elif move.pieceMoved == 'bK':
+                self.blackKingLocation = (move.endRow, move.endCol)
