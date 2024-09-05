9053c8fa59fac77f60372c911d63ec830003a4d2
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-09-05 17:28:56 +0530

    Update ChessMain.py

--- CHESS/ChessMain.py
+++ CHESS/ChessMain.py
@@ -6,3 +6,8 @@ DIMENTIONS = 8
 SQ_SIZE = HEIGHT // DIMENTIONS
 MAX_FPS = 15
 IMAGES = {}
+
+def loadimages():
+    pieces = ['bB', 'bK', 'bN', 'bp', 'bQ', 'bR', 'wB', 'wK', 'wN', 'wp', 'wQ', 'wR']
+    for piece in pieces:
+        IMAGES[piece] = p.image.load("my_chess/images/" + piece + ".png")
