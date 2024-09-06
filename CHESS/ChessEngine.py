7314f6f4913b4e5f590ed56fc0cac05f2a7c135d
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-09-06 17:59:41 +0530

    Update ChessEngine.py

--- CHESS/ChessEngine.py
+++ CHESS/ChessEngine.py
@@ -10,16 +10,46 @@ class gamestate():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        
+        self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N': self.getKnightMoves,
+                              'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
+        self.whitetomove = True
+        self.movelog = []
 
     def makeMoves(self, move):
-        if len(self.movelog)!=0:
+        if len(self.movelog)!=0:    
             self.board[move.endRow][move.endCol] = move.pieceMoved
             self.board[move.startRow][move.startCol] = "--"
-            self.movelog.append(move)
+            self.movelog.append(move) 
             self.whitetomove = not self.whitetomove  # Toggle turn
-            print("Move made:", move.getChessnotation())
-            self.printBoard()
             if move.pieceMoved == 'wK':
                 self.whiteKingLocation = (move.endRow, move.endCol)
             elif move.pieceMoved == 'bK':
                 self.blackKingLocation = (move.endRow, move.endCol)
+                
+            if move.isPawnPromotion:
+                 self.board[move.endRow][move.endCol] = move.pieceMoved[0] + 'Q' 
+            if move.isEnpassantMOve:
+                self.board[move.startRow][move.endCol] = '--'
+                
+            if move.pieceMoved[1] == 'p' and abs(move.startRow - move.endRow) == 2:
+                self.enpassantPossible =((move.startRow + move.endRow)//2, move.startCol)
+            else:
+                self.enpassantPossible =()    
+
+    def undoMove(self):
+        if len(self.movelog) != 0:
+            move = self.movelog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whitetomove = not self.whitetomove  # Toggle turn back
+            if move.pieceMoved == 'wK':
+                self.whiteKingLocation = (move.startRow, move.startCol)
+            elif move.pieceMoved == 'bK':
+                self.blackKingLocation = (move.startRow, move.startCol)
+            if move.isEnpassantMove:
+                self.board[move.endRow][move.endCol] = '--'
+                self.board[move.startRow][move.startCol] = move.pieceCaptured
+                self.enpassantPossible = (move.endRow, move.endCol)
+            if move.pieceMoved[1]== 'p' and abs(move.startRow- move.endRow) == 2:
+                self.enpassantPossible =()  
