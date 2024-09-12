394db0b710ce1fd03a6612d53fe3858d3d7fd3c0
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-09-12 16:14:25 +0530

    Update ChessEngine.py

--- CHESS/ChessEngine.py
+++ CHESS/ChessEngine.py
@@ -146,3 +146,33 @@ class gamestate():
                             break
                 else:
                     break
+
+    def getKnightMoves(self, r, c, moves):
+        knightMoves = ((-2, -1), (-2, 1),(-1, -2), (-1, 2), (1,-2), (1, 2), (2,-1), (2, 1))
+        allyColor = "w" if self.whitetomove else "b"
+        for m in knightMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
+
+    def getBishopMoves(self, r, c, moves):
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemyColor = "b" if self.whitetomove else "w"
+        for d in directions:
+            for i in range(1, 8):
+                endRow = r + d[0] * i
+                endCol = c + d[1] * i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:
+                            break
+                else:
+                    break
